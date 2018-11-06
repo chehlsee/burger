84efe0f108abca931374274a28fa5a9287f82796
Chehlsee <chelseaciprich@gmail.com>
2018-11-06 11:37:50 -0500

    create and update all files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('BBQ Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Avocado Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Black Bean Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (name) VALUES ('Turkey Burger');
