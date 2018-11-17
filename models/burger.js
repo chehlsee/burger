ce1fa773563241fda4bef83870e66f58111e5391
Chehlsee <chelseaciprich@gmail.com>
2018-11-17 12:57:53 -0500

    fixing errors

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,6 @@
 // Import the ORM to create functions that will interact with the database.
 // same as in class cat app assignment 
-var orm = require("../config/orm.js");
+var orm = require("../config/orm");
 
 var burger = {
   all: function(cb) {
